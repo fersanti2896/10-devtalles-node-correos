6fd5459f8ffd50df96815d8eae15598007ef8173
Fernando Nicolás <fersanti2896@gmail.com>
2023-11-13 14:46:08 -0600

    Envío de correos.

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,7 @@
-import { CheckService } from '../domain/use-cases/checks/check-service';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
 
 /* Instancias */
 const fileSystemLogRepository = new LogRepositoryImpl( 
@@ -11,19 +11,25 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class Server {
     public static start() {
         console.log('Server started...')
-        
-        CronService.createJob( 
-            '*/5 * * * * *',
-            () => {
-                const url = 'https://google.com';
 
-                new CheckService(
-                    fileSystemLogRepository,
-                    () => console.log(`${ url } is ok`),
-                    ( error ) => console.log(error)
-                ).execute( url );
-                // new CheckService().execute('http://localhost:3000');
-            }
+        /* Mandando email */
+        const emailService = new EmailService(fileSystemLogRepository)
+        emailService.sendEmailWithFileSystemLogs(
+            ['fersanti2896@gmail.com']
         );
+        
+        // CronService.createJob( 
+        //     '*/5 * * * * *',
+        //     () => {
+        //         const url = 'https://google.com';
+
+        //         new CheckService(
+        //             fileSystemLogRepository,
+        //             () => console.log(`${ url } is ok`),
+        //             ( error ) => console.log(error)
+        //         ).execute( url );
+        //         // new CheckService().execute('http://localhost:3000');
+        //     }
+        // );
     }   
 }
